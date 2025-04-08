89795750f35a204c107295bafa0851c52cc55193
HBanty <bijnadatta@gmail.com>
2025-04-08 21:47:26 +0530

    Update Home component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -32,7 +32,7 @@ function Home() {
    useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const response = await axios.get("http://localhost:4001/api/v1/course/courses",
+        const response = await axios.get(`${BACKEND_URL}/course/courses`,
           {
             withCredentials: true,
           }
