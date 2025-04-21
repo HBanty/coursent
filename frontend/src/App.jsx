ed3127a552f5ca3ee5e5145331f901b502a2b835
HBanty <bijnadatta@gmail.com>
2025-04-22 05:29:32 +0530

    update purchaseroute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,54 @@
-import React from 'react'
-import { Navigate, Route, Routes} from 'react-router-dom'
-import Home from './components/Home'
-import Login from './components/Login'
-import Signup from './components/Signup'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
-import  { Toaster } from 'react-hot-toast';
-import Courses from './components/Courses'
-import Buy from './components/Buy'
-import Purchases from './components/Purchases'
-import AdminLogin from './admin/AdminLogin'
-import AdminSignup from './admin/AdminSignup'
-import CourseCreate from './admin/CourseCreate'
-import UpdateCourse from './admin/UpdateCourse'
-import OurCourses from './admin/OurCourses'
-import Dashboard from './admin/Dashboard'
+import { Toaster } from "react-hot-toast";
+import Courses from "./components/Courses";
+import Buy from "./components/Buy";
+import Purchases from "./components/Purchases";
+import AdminLogin from "./admin/AdminLogin";
+import AdminSignup from "./admin/AdminSignup";
+import CourseCreate from "./admin/CourseCreate";
+import UpdateCourse from "./admin/UpdateCourse";
+import OurCourses from "./admin/OurCourses";
+import Dashboard from "./admin/Dashboard";
 
 function App() {
   const user = JSON.parse(localStorage.getItem("user"));
   const admin = JSON.parse(localStorage.getItem("admin"));
   return (
     <div>
-          <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        
-       {/* Other Routes */}
-       <Route path="/courses" element={<Courses />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* Other Routes */}
+        <Route path="/courses" element={<Courses />} />
         <Route path="/buy/:courseId" element={<Buy />} />
-        <Route path="/purchases" element={<Purchases/>}
-        />
-{/*         you can use below one if required 
- <Route
+        {/* <Route path="/purchases" element={<Purchases/>}
+        /> */}
+        you can use below one if required
+        <Route
           path="/purchases"
           element={user ? <Purchases /> : <Navigate to={"/login"} />}
-        />*/}
+        />
+        {/* Admin routes */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/signup" element={<AdminSignup />} />
+        <Route path="/admin/create-course" element={<CourseCreate />} />
+        <Route path="/admin/update-course/:id" element={<UpdateCourse />} />
+        <Route path="/admin/our-courses" element={<OurCourses />} />
+        <Route
+          path="/admin/dashboard"
+          element={admin ? <Dashboard /> : <Navigate to={"/admin/login"} />}
+        />
+      </Routes>
 
-          {/* Admin routes */}
-          <Route path="/admin/login" element={<AdminLogin/>} />
-          <Route path="/admin/signup" element={<AdminSignup/>} />
-          <Route path="/admin/create-course" element={<CourseCreate/>} />
-          <Route path="/admin/update-course/:id" element={<UpdateCourse/>} />
-          <Route path="/admin/our-courses" element={<OurCourses/>} />
-          <Route path="/admin/dashboard" element={admin?<Dashboard/> :<Navigate to ={"/admin/login"}/>} />
-          </Routes> 
-
-          <Toaster />
+      <Toaster />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
